1d7f5cb918516a14960f4e0ea997336d7218f657
Amit Mahendru <amitmahendru92@gmail.com>
2022-07-16 16:29:06 -0400

    Time: 4 ms (33.96%), Space: 53.5 MB (76.94%) - LeetHub

---
+++ 702-search-in-a-sorted-array-of-unknown-size/702-search-in-a-sorted-array-of-unknown-size.java
@@ -0,0 +1,32 @@
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface ArrayReader {
+ *     public int get(int index) {}
+ * }
+ */
+
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        if(reader.get(0)==target)
+            return 0;
+        int left=0;
+        int right=1;
+        while(reader.get(right)<target){
+            left = right;
+            right*=2;
+        }
+        int mid=0;
+        while(left<=right){
+            mid= (left+right)/2;
+            if(reader.get(mid)==target)
+                return mid;
+            if(reader.get(mid)>target)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        return -1;
+        
+    }
+}
