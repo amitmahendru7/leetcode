42352ad6a41e307adaeb81257da7ee61daa923da
Amit Mahendru <amitmahendru92@gmail.com>
2022-07-17 12:26:31 -0400

    Time: 4 ms (30.61%), Space: 42.2 MB (31.71%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,29 +1,25 @@
 class Solution {
+    private HashMap<Character, Character> mappings;
+    public Solution(){
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')','(');
+        this.mappings.put('}','{');
+        this.mappings.put(']','[');
+    }
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='[')
-                stack.push(s.charAt(i));
-            if(s.charAt(i)==')'){
-                if(!stack.isEmpty() && stack.peek()=='(')
-                    stack.pop();
-                else
-                    stack.push(s.charAt(i));
-                
-            }else if(s.charAt(i)==']'){
-                if(!stack.isEmpty() && stack.peek()=='[')
-                    stack.pop();
-                else
-                    stack.push(s.charAt(i));
-            }else if(s.charAt(i)=='}'){
-                if(!stack.isEmpty() && stack.peek()=='{')
-                    stack.pop();
-                else
-                    stack.push(s.charAt(i));
+            char c= s.charAt(i);
+            //Check for the CLosing bracket
+            if(this.mappings.containsKey(c)){
+                char topElement = stack.isEmpty() ? '#': stack.pop();
+                if(topElement != this.mappings.get(c))
+                    return false;
+            }
+            else{
+                stack.push(c);
             }
         }
-        if(stack.size()==0)
-            return true;
-        return false;
+        return stack.isEmpty();
     }
 }
